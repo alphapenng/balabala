9ccecf5101583a52bc706394be3a80170ac6158e
alphapenng <barca8best@gmail.com>
2021-08-28 23:05:26 +0800

    2021-08-28

--- content/posts/my-first-post.md
+++ content/posts/my-first-post.md
@@ -1,7 +1,7 @@
 ---
 title: "My First Post"
 date: 2021-08-27T21:52:14+08:00
-draft: false
+draft: true
 toc: false
 isCJKLanguage: true
 images: 
--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd ../alphapenng.github.io/
 git config user.name “alphapenng”
 git config user.email “barca8best@gmail.com
 
-git add --ignore-removal . && git commit -m 'fix the size of pictures in "版本管理与Git入门"' && git push -u origin main
+git add --ignore-removal . && git commit -m 'fix typo in Git 入门' && git push -u origin main
 
 cd ../balabala/
