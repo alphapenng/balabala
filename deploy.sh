65b327a2c47c87640e71469b4a5980ccb0123e87
彭晓勇 <alphapenng@MacBook-Pro.local>
2021-08-28 17:17:53 +0800

    auto launch blog with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ hugo
 
 cp -rf public/* ../alphapenng.github.io/docs/
 
-cd ../polaris1119.github.io/
+cd ../alphapenng.github.io/
 
 git add * && git commit -m 'new article' && git push
 
-cd ../polarisxu/
+cd ../balabala/
