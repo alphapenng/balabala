79cc57d158adb3aa19f1b82fcecd8f69a79f25eb
彭晓勇 <alphapenng@MacBook-Pro.local>
2021-08-28 17:48:22 +0800

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 
 hugo
 
-cp -rf public/* ../alphapenng.github.io/docs/
+cp -Rf public/* ../alphapenng.github.io/docs/
 
 cd ../alphapenng.github.io/
 
-git add * && git commit -m 'new article' && git push
+git config user.name “alphapenng”
+git config user.email “barca8best@gmail.com
+
+git add --ignore-removal . && git commit -m 'new article' && git push -u origin main
 
 cd ../balabala/
