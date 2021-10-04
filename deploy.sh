72508f0b13476da7763c792d2bbaba04f8c58930
alphapenng <barca8best@gmail.com>
2021-10-04 12:28:19 +0800

    edit title of 「华为路由交换设备命令手册」

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd ../alphapenng.github.io/
 git config user.name “alphapenng”
 git config user.email “barca8best@gmail.com
 
-git add --ignore-removal . && git commit -m 'add new post 「华为路由交换设备命令手册」' && git push -u origin main
+git add --ignore-removal . && git commit -m 'edit title 「华为路由交换设备命令手册」' && git push -u origin main
 
 cd ../balabala/
